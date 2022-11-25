dca030725c05d72ba319bd050d192ca6a92a76e2
a123 <shchevelev.roman@yandex.ru>
2022-11-25 13:23:36 +0300

    Задача 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача №1
+        System.out.println("Задача №1");
+        int salary = 15_000;
+        double total = 0;
+        int i = 0;
+        for ( ; total < 2_459_000; i ++) {
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+        //Задача №2
+        System.out.println("Задача №2");
+        int z = 0;
+        while (z < 10){
+            z++;
+            System.out.print(z + " ");
+        }
+        System.out.println("");
+        int x = 11;
+        while (x >1){
+            x--;
+            System.out.print(x + " ");
+        }
     }
 }
