51c1810cc03d0a3096bb3aa4a6b9d1ba710320aa
a123 <shchevelev.roman@yandex.ru>
2022-11-27 15:55:34 +0300

    Задача 1-6 4-6 требует проверки

--- src/Main.java
+++ src/Main.java
@@ -36,10 +36,39 @@ public class Main {
         // Задача №4
         System.out.println("Задача №4");
         double startDeposit = 15_000;
-        for (int mouth = 0;startDeposit <= 12_000_000 ; mouth++){
+        for (int mouth = 1;startDeposit <= 12_000_000 ; mouth++) {
             startDeposit = startDeposit + startDeposit * 0.07;
             System.out.println("Месяц " + mouth + " сумма " + startDeposit);
         }
+        // Задача №5
+        System.out.println("Задача №5");
+        double totalDeposit = 15_000;
+        for (int q = 1;totalDeposit <= 12_000_000 ; q++) {
+            totalDeposit = totalDeposit + totalDeposit * 0.07;
+            if (q % 6 == 0) {
+                System.out.println("Месяц " + q + " сумма " + totalDeposit);
+            }
+        }
+        //Задача №6
+        System.out.println("Задача №6");
+        double startSum = 15_000;
+        for (int report = 1; report < (9 * 12) ; report++) {
+            startSum = startSum + startSum * 0.07;
+            if ( report % 6 == 0){
+                System.out.println("По истечению " + report + " месяцев, сумма накоплений будет состовлять " + startSum);
+            }
+        }
+        // Задача №7
+        System.out.println("Задача №7");
+        int fryday = 7;
+        System.out.println("Сегодня пятница, " + fryday + " -е число. Необходимо подготовить отчет.");
+        for ( int day = 0; day < 31; day = fryday + 7 ){
+            fryday = fryday + 7;
+            if (fryday < 31)
+                System.out.println("Сегодня пятница, " + fryday + " -е число. Необходимо подготовить отчет.");
+            }
+        //Задача №8
+        System.out.println("Задача №8");
 
     }
 }
